491d29c32570e24160c1f2d75b2b34f10eb92db8
Sam Baskin <sambskn@gmail.com>
2025-07-13 13:20:44 -0600

    color changing indicator

--- src/text_boxes.rs
+++ src/text_boxes.rs
@@ -10,6 +10,10 @@ pub struct TextBox;
 
 #[derive(Component, Reflect, Default)]
 #[reflect(Component)]
+pub struct TextBoxIndicator;
+
+#[derive(Component, Reflect, Default)]
+#[reflect(Component)]
 pub struct TextBoxText {
     pub is_visible: bool,
     pub visible_color: Color,
@@ -58,10 +62,12 @@ impl TextBoxMesh {
 pub(super) fn plugin(app: &mut App) {
     app.register_type::<TextBox>();
     app.register_type::<TextBoxMesh>();
-    app.register_type::<TextBoxMesh>();
+    app.register_type::<TextBoxText>();
+    app.register_type::<TextBoxIndicator>();
 
     app.add_systems(Update, animate_text_box_mesh_intro);
     app.add_systems(Update, animate_text_box_text_intro);
+    app.add_systems(Update, animate_text_box_indicator);
 }
 
 fn animate_text_box_mesh_intro(
@@ -84,6 +90,24 @@ fn animate_text_box_mesh_intro(
         let new_mesh = get_text_box_mesh(mesh_info.with_cutout, alpha);
         let new_mesh_handle = meshes.add(new_mesh);
         mesh2d.0 = new_mesh_handle;
+    }
+}
+
+const TRIANGLE_WOBBLE_SPEED: f32 = 4.;
+const TRIANGLE_WOBBLE_OFFSET: f32 = 3.;
+fn animate_text_box_indicator(
+    mut mesh2d_query: Query<(&mut Mesh2d, &mut Transform), With<TextBoxIndicator>>,
+    mut meshes: ResMut<Assets<Mesh>>,
+    time: Res<Time>,
+) {
+    for (mut mesh2d, mut transform) in mesh2d_query.iter_mut() {
+        let t = (time.elapsed_secs() * TRIANGLE_WOBBLE_SPEED).sin();
+        let new_mesh =
+            get_colored_triangle_mesh(TRIANGLE_INDICATOR_HEIGHT, TRIANGLE_INDICATOR_WIDTH, t);
+        let new_mesh_handle = meshes.add(new_mesh);
+        mesh2d.0 = new_mesh_handle;
+        transform.translation.y =
+            TEXTBOX_OFFSET_FROM_CENTER_Y - (TEXTBOX_HEIGHT / 2.0) + t * TRIANGLE_WOBBLE_OFFSET;
     }
 }
 
@@ -116,10 +140,14 @@ pub const TEXTBOX_BG_SHADOW_OFFSET: f32 = 8.0;
 pub const TEXTBOX_WIDTH: f32 = 700.;
 pub const TEXTBOX_HEIGHT: f32 = 200.;
 
+pub const TRIANGLE_INDICATOR_WIDTH: f32 = 34.;
+pub const TRIANGLE_INDICATOR_HEIGHT: f32 = 28.;
+
 pub const BOX_BG_Z: f32 = 1.;
 pub const BOX_BG_SHADOW_Z: f32 = 0.9;
 pub const TEXT_SHADOW_Z: f32 = 1.1;
 pub const TEXT_Z: f32 = 1.2;
+pub const TRIANGLE_MESH_Z: f32 = 1.3;
 
 pub const TEXT_SHADOW_OFFSET: f32 = 2.0;
 pub const TEXT_FONT_SIZE: f32 = 25.0;
@@ -136,10 +164,14 @@ pub fn text_box(
 ) -> impl Bundle {
     let main_bg_mesh = get_text_box_mesh(true, 0.0);
     let bg_shadow_mesh = get_text_box_mesh(false, 0.0);
-
+    let triangle_mesh = get_colored_triangle_mesh(
+        TRIANGLE_INDICATOR_HEIGHT,
+        TRIANGLE_INDICATOR_WIDTH,
+        spawn_time.sin(),
+    );
     let main_bg_mesh_handle = meshes.add(main_bg_mesh);
     let bg_shadow_mesh_handle = meshes.add(bg_shadow_mesh);
-
+    let triangle_mesh_handle = meshes.add(triangle_mesh);
     (
         TextBox,
         children![
@@ -192,6 +224,17 @@ pub fn text_box(
                 TextColor(GHOST_WHITE.into()),
                 TextBoxText::new(TEXTBOX_FADE_IN_TIME, spawn_time, GHOST_WHITE.into())
             ),
+            (
+                Mesh2d(triangle_mesh_handle),
+                MeshMaterial2d(materials.add(ColorMaterial::default())),
+                Transform::from_translation(Vec3::new(
+                    0.,
+                    TEXTBOX_OFFSET_FROM_CENTER_Y - (TEXTBOX_HEIGHT / 2.0),
+                    TRIANGLE_MESH_Z
+                ))
+                .with_scale(Vec3::splat(1.)),
+                TextBoxIndicator,
+            ),
         ],
     )
 }
@@ -242,5 +285,45 @@ fn get_text_box_mesh(with_inner_vertices: bool, alpha: f32) -> Mesh {
             LinearRgba::new(0.92, 0.1, 0.1, alpha).to_f32_array(),
         ]);
     }
+    mesh.with_inserted_attribute(Mesh::ATTRIBUTE_COLOR, vertex_colors)
+}
+
+fn get_colored_triangle_mesh(height: f32, width: f32, t: f32) -> Mesh {
+    let half_height = height / 2.0;
+    let half_width = width / 2.0;
+    let vertices = vec![
+        [-half_width, half_height, 0.0],
+        [half_width, half_height, 0.0],
+        [0.0, -half_height, 0.0],
+    ];
+    let mesh = Mesh::new(
+        PrimitiveTopology::TriangleList,
+        RenderAssetUsages::MAIN_WORLD | RenderAssetUsages::RENDER_WORLD,
+    )
+    .with_inserted_attribute(Mesh::ATTRIBUTE_POSITION, vertices)
+    .with_inserted_indices(Indices::U32(vec![0, 1, 2]));
+    // Build vertex colors for the triangle. One entry per vertex (the corners of the triangle)
+
+    let color1 = if t >= 0. {
+        LinearRgba::new(0.05, 0.2, 0.95, 1.0).mix(&LinearRgba::new(0.05, 0.95, 0.2, 1.0), t)
+    } else {
+        LinearRgba::new(0.05, 0.2, 0.95, 1.0).mix(&LinearRgba::new(0.95, 0.95, 0.2, 1.0), t.abs())
+    };
+    let color2 = if t >= 0. {
+        LinearRgba::new(0.0, 0.17, 0.97, 1.0).mix(&LinearRgba::new(0.0, 0.97, 0.17, 1.0), t)
+    } else {
+        LinearRgba::new(0.0, 0.17, 0.97, 1.0).mix(&LinearRgba::new(0.97, 0.97, 0.17, 1.0), t.abs())
+    };
+    let color3 = if t >= 0. {
+        LinearRgba::new(0.0, 0.1, 0.98, 1.0).mix(&LinearRgba::new(0.0, 0.98, 0.1, 1.0), t)
+    } else {
+        LinearRgba::new(0.0, 0.1, 0.98, 1.0).mix(&LinearRgba::new(0.98, 0.98, 0.1, 1.0), t.abs())
+    };
+
+    let vertex_colors: Vec<[f32; 4]> = vec![
+        color1.to_f32_array(),
+        color2.to_f32_array(),
+        color3.to_f32_array(),
+    ];
     mesh.with_inserted_attribute(Mesh::ATTRIBUTE_COLOR, vertex_colors)
 }
